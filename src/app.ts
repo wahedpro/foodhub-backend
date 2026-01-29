85f5bb8c8daa8010c7b8a1edcd5ae5ec55d536ad
Wahedul Islam <wahedulislam.pro@gmail.com>
2026-01-29 21:08:57 +0600

    update database url and enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.route";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
