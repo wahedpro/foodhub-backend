83d702a3eddb1ba8ab6b12bd27d89b7509a75192
Wahedul Islam <wahedulislam.pro@gmail.com>
2026-02-02 20:45:24 +0600

    refactor: remove changePassword route and associated logic from user controller

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import {
   updateProfile,
-  changePassword,
 } from "./user.controller";
 import { auth } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.patch("/me", auth, updateProfile);
-router.patch("/change-password", auth, changePassword);
+
 
 export default router;
